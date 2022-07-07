c84ccd38f3e5dac8dcd9485e7b26413c75743be8
danielOuattara <daniel.ouattara@gmx.com>
2022-07-07 15:51:06 +0200

    adding data.js

---
+++ 13_type_relations/server/schema/data.js
@@ -0,0 +1,63 @@
+// dummy data
+
+//--------------------------------------------------
+const books = [
+  {
+    name: "Name of the wind",
+    genre: "Fantasy",
+    id: "1",
+    pages: "230",
+    authorId: "1",
+  },
+  {
+    name: "The final empire",
+    genre: "Fantasy",
+    id: "2",
+    pages: "340",
+    authorId: "2",
+  },
+  {
+    name: "The long earth",
+    genre: "Sci-Fi",
+    id: "3",
+    pages: "520",
+    authorId: "3",
+  },
+  {
+    name: "Dance the stars",
+    genre: "Novel",
+    id: "4",
+    pages: "320",
+    authorId: "3",
+  },
+  {
+    name: "The guitar play ",
+    genre: "Music",
+    id: "5",
+    pages: "270",
+    authorId: "1",
+  },
+  {
+    name: "Swim in the sea",
+    genre: "Practics",
+    id: "6",
+    pages: "220",
+    authorId: "2",
+  },
+  {
+    name: "Fight the vaccin",
+    genre: "Novel",
+    id: "7",
+    pages: "300",
+    authorId: "2",
+  },
+];
+
+//--------------------------------------------------
+const authors = [
+  { id: "1", name: "Patrick Rothfus", age: "44" },
+  { id: "2", name: "Bradon Sanderso", age: "42" },
+  { id: "3", name: "Terry Pratchett", age: "66" },
+];
+
+module.exports = { authors, books };
