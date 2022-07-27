ee64e96f5a93da625c68fb3c576e0612835d32ff
danielOuattara <daniel.ouattara@gmx.com>
2022-07-27 16:24:34 +0200

    updating frontend to new appolo/client logic

--- 25_appollo_client_setup/frontend/src/App.js
+++ 25_appollo_client_setup/frontend/src/App.js
@@ -1,25 +1,27 @@
-
 import { Component } from "react";
-import ApolloClient from "apollo-boost";
-import  { ApolloProvider } from "react-apollo"; 
 import BookList from "./components/BookList";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 // apollo-client setup
-const client = new ApolloClient ({
+const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
-})
+  cache: new InMemoryCache(),
+});
 
 export default class App extends Component {
   render() {
     return (
-       <ApolloProvider client={client}>
+      <ApolloProvider client={client}>
         <div id="main">
           <h1> Ninja's Reading List </h1>
           <BookList />
         </div>
-       </ApolloProvider>
+      </ApolloProvider>
     );
   }
 }
-
