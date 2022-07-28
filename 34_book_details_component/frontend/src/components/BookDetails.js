b0a46bbce9cd4db71a7c494c7623367c8181ff55
danielOuattara <daniel.ouattara@gmx.com>
2022-07-28 18:48:12 +0200

    book details component new update

--- 34_book_details_component/frontend/src/components/BookDetails.js
+++ 34_book_details_component/frontend/src/components/BookDetails.js
@@ -1,18 +1,17 @@
+import { Component } from "react";
+import { graphql } from "@apollo/client/react/hoc";
+import { GETONEBOOK } from "./../queries/queries";
+//----------------------------------------------------
 
-import React, { Component } from 'react';
-import {graphql} from "react-apollo";
-import { getOneBookQuery } from "./../queries/queries";
-
- class BookDetails extends Component {
-
-    render() {
-        console.log(this.props)
-        return (
-            <div id="book-details">
-               <p>Output book details here !</p>
-            </div>
-        )
-    }
+class BookDetails extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div id="book-details">
+        <p>Output book details here !</p>
+      </div>
+    );
+  }
 }
 
-export default graphql(getOneBookQuery)(BookDetails);
+export default graphql(GETONEBOOK)(BookDetails);
