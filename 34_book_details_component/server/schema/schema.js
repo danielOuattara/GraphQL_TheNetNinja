fdf63693a78c0b4470458019ce051188adc886d3
danielOuattara <daniel.ouattara@gmx.com>
2022-07-28 18:45:30 +0200

    copy from previous server

--- 34_book_details_component/server/schema/schema.js
+++ 34_book_details_component/server/schema/schema.js
@@ -16,6 +16,24 @@ const {
 
 } = graphql;
 
+
+// dummy data
+// const books = [
+//     { title: "Name of the wind", genre: "Fantasy",  id: "1", pages: "230", authorId: "1" },
+//     { title: "The final empire", genre: "Fantasy",  id: "2", pages: "340", authorId: "2" },
+//     { title: "The long earth",   genre: "Sci-Fi",   id: "3", pages: "520", authorId: "3" },
+//     { title: "Dance the stars",  genre: "Novel",    id: "4", pages: "320", authorId: "3" },
+//     { title: "The guitar play ", genre: "Music",    id: "5", pages: "270", authorId: "1" },
+//     { title: "Swim in the sea",  genre: "Practics", id: "6", pages: "220", authorId: "2" },
+//     { title: "Fight the vaccin", genre: "Novel",    id: "7", pages: "300", authorId: "2" },
+// ];
+
+// const authors = [
+//     { name: "Patrick Rothfus", age: "44", id: "1" },
+//     { name: "Bradon Sanderso", age: "42", id: "2" },
+//     { name: "Terry Pratchett", age: "66", id: "3" },
+// ];
+
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
@@ -137,6 +155,7 @@ const Mutation = new GraphQLObjectType ({
         },
     }
 })
+
 
 module.exports = new GraphQLSchema({
     query: RootQuery,
