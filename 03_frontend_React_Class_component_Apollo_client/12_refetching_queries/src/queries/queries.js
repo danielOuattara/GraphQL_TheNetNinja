9e0c2a8954c714656a5a8c9a4b4081e636b7b8b7
danielOuattara <daniel.ouattara@gmx.com>
2023-08-24 23:46:17 +0200

    03-12 refetching queries

---
+++ 03_frontend_React_Class_component_Apollo_client/12_refetching_queries/src/queries/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+//---------------------------------------------
+
+const GETAUTHORS = gql`
+  query GetAuthors {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+//--------------------------------------------
+const GETBOOKS = gql`
+  query GetBooks {
+    books {
+      title
+      id
+    }
+  }
+`;
+
+//--------------------------------------------
+// query variable in mutation below, starting with $ sign
+const ADDBOOK_MUTATION = gql`
+  mutation AddBook(
+    $title: String!
+    $genre: String!
+    $authorId: ID!
+    $pages: Int!
+  ) {
+    addBook(title: $title, genre: $genre, authorId: $authorId, pages: $pages) {
+      id
+      title
+      pages
+      author {
+        name
+        age
+      }
+    }
+  }
+`;
+
+//--------------------------------------------
+export { GETAUTHORS, GETBOOKS, ADDBOOK_MUTATION };
