54d58062e25c47064c060d707ea229f40a74c75d
danielOuattara <daniel.ouattara@gmx.com>
2023-08-24 23:43:17 +0200

    03-03 books list component

---
+++ 03_frontend_React_Class_component_Apollo_client/03_books_list_component/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import BookList from "./components/BookList";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1> Ninja's Reading List </h1>
+        <BookList />
+      </div>
+    );
+  }
+}
---
+++ 03_frontend_React_Class_component_Apollo_client/03_books_list_component/src/components/BookList.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name 1</li>
+          <li>Book name 2</li>
+        </ul>
+      </div>
+    );
+  }
+}
