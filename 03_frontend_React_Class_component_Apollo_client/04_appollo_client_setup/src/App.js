3ecac65f2e1086d07e7eb6dd86821cf9c9519f46
danielOuattara <daniel.ouattara@gmx.com>
2023-08-24 23:43:33 +0200

    03-04 apollo client setup

---
+++ 03_frontend_React_Class_component_Apollo_client/04_appollo_client_setup/src/App.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import BookList from "./components/BookList";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// apollo-client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1> Ninja's Reading List </h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
