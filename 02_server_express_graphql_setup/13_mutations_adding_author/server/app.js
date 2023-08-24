c9309b5e86dc4235c3a3b4935a4cf45b6b64c8f6
danielOuattara <daniel.ouattara@gmx.com>
2023-08-24 23:39:04 +0200

    02 -13 mutations adding author

---
+++ 02_server_express_graphql_setup/13_mutations_adding_author/server/app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema.js");
+const mongoose = require("mongoose");
+const app = express();
+//-------------------------------------------------------
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
+
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Successful" });
+});
+
+//-------------------------------------------------------
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(
+      `Success: Database connected to: ${process.env.DATABASE} database * * * `,
+    );
+    app.listen(4000, () => {
+      console.log("Listening on http://localhost:4000/graphql?");
+    });
+  })
+  .catch((err) => console.log(err.message));
